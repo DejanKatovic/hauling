8cd42839503c6413717e0afab2b3aca452ecc2c8
DejanKatovic <dejankatovic@gmail.com>
2022-03-01 03:49:39 -0800

    Test for navtrac

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import CheckoutPage from "./Components/CheckoutPage"
+import MaterialLayout from './Components/Layout/MaterialLayout.jsx';
+import Register from  "./Components/Register"
+
+export default function Home() {
+  
+  return (
+    // <MaterialLayout>
+    //   <CheckoutPage />
+    // </MaterialLayout>
+    <Register/>
+  )
+
+}
