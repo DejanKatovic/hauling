ac2992bf111ee11d4d7f6a4e7ce76537346a1c90
DejanKatovic <dejankatovic@gmail.com>
2022-03-01 03:59:54 -0800

    Update the code styles

--- pages/api/checkin.js
+++ pages/api/checkin.js
@@ -1,13 +1,11 @@
+const usersData = [];
 
-let usersData = [];
-
-export default function handler(req, res) {  
-	if (req.method !== 'POST') {
-    return res.status(405).send({ message: 'Only POST requests allowed' })
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send({ message: "Only POST requests allowed" });
   }
 
   const body = req.body;
   usersData.push(body);
-	res.status(200).json(body);
+  res.status(200).json(body);
 }
-  
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
-import CheckoutPage from "./Components/CheckoutPage"
-import MaterialLayout from './Components/Layout/MaterialLayout.jsx';
-import Register from  "./Components/Register"
+import Register from "./Components/Register";
 
 export default function Home() {
-  
-  return (
-    // <MaterialLayout>
-    //   <CheckoutPage />
-    // </MaterialLayout>
-    <Register/>
-  )
-
+  return <Register />;
 }
